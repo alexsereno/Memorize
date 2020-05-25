bf5bb7c06f3ee7f03910f68c00e42bcedf7b3a95
Alex Sereno <2569805+alex@users.noreply.github.com>
2020-05-24 19:43:32 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Alexander Sereno on 5/24/20.
+//  Copyright © 2020 Alexander Sereno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
