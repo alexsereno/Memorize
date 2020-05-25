52ac842e0ee57df63b707c5575d212c2f17a9a63
Alex Sereno <2569805+alex@users.noreply.github.com>
2020-05-24 21:11:46 -0500

    Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    //curly braces mean the internal value is computed
     var body: some View {
-        Text("Hi!")
+        //there is an implicit "return" in all one line {}
+        //return Text("")
+        
+        //HStach - stacks horizontally, has default spacing between stacks
+        HStack {
+            //foreach takes an iteror - ideally a list, and creates views for each lul
+            ForEach(0..<4) { _ in
+                CardView(isFaceUp: false)
+            }
+        }
+            //applying a function to the Z stack applies to all contained views
+            //you can ovverride inherited functions contained views
+            .foregroundColor(Color.orange)
+            .padding()
+            .font(Font.largeTitle)
     }
 }
+
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                //inherting color from Z-stack
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
